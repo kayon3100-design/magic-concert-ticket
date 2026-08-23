e433a6990f5d467c74b224a254e2521fd8f32415
kayon3100-design <kayon3100@gmail.com>
2026-08-23 15:24:35 +0700

    Refresh PWA shell cache after curtain fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='magic-ticket-shell-v1';
+const CACHE='magic-ticket-shell-v2';
 const SHELL=['./','./index.html','./styles.css','./app.js','./config.js','./pwa.css','./pwa.js','./manifest.webmanifest','./magic-ticket-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
